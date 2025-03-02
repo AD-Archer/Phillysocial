945759858f9181d94c6722aeb47430f62fb8ed97
Archer <89430358+AD-Archer@users.noreply.github.com>
2025-03-01 21:43:17 -0500

    Update to unblur footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#003940] text-white p-4 shadow-lg">
-      <p className="text-center">© 2024 Philly Social. All rights reserved.</p>
+    <footer className="bg-[#003940] text-white p-4 shadow-lg relative z-10 w-full">
+      <div className="container mx-auto">
+        <p className="text-center">© 2024 Philly Social. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
